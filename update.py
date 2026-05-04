0f8bf54c7c634850d0a5d9a74b64fc18241b92a8
Tony <tonyyang749@gmail.com>
2026-05-04 16:34:10 +0200

    added fix to update.py file

--- update.py
+++ update.py
@@ -14,7 +14,7 @@ def get_curr_version():
     else:
         base_path = os.path.dirname(__file__)
 
-    version_file = os.path.join(base_path)
+    version_file = os.path.join(base_path, "version.txt")
     with open(version_file, "r") as f:
         return f.read().strip()
     
@@ -26,13 +26,13 @@ def check_for_update():
         new_exe = sys.executable + ".new"
         with open(new_exe, "wb") as f:
             f.write(requests.get(DOWNLOAD_URL).content)
-        bat = os.path.join(os.path_dirname(sys.executable), "update.bat")
+        bat = os.path.join(os.path.dirname(sys.executable), "update.bat")
         with open(bat, "w") as f:
             f.write(f'@echo off\ntimeout /t 2 /nobreak\nmove /y "{new_exe}" "{sys.executable}"\nstart "" "{sys.executable}"\ndel "%~f0"')
         subprocess.Popen(bat, shell=True)
         sys.exit()
-    except Exception:
-        pass
+    except Exception as e:
+        print(e)
     
 if getattr(sys,'frozen', False):
     check_for_update()
